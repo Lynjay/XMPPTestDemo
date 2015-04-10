bdd475bd6c1fd6c591094384464d03a4c576bd0f
Lynjay <apple@appledeiMac.local>
2015-04-10 10:34:41 +0800

    上传工程
    1.没修改什么，就是上传工程而已

---
+++ XMPPTestDemo/XMPPTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPTestDemo
+//
+//  Created by gwchina on 15/4/10.
+//  Copyright (c) 2015年 绿网天下. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPTestDemo/XMPPTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPTestDemo
+//
+//  Created by gwchina on 15/4/10.
+//  Copyright (c) 2015年 绿网天下. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
